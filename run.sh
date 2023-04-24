b1541c3e7406a6d2d73e4a00e99b77956830bf3a
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2023-04-24 20:52:23 +0530

    pid kill process 3000

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 echo "Node Server of HMS-T3-Backend"
+# Get the PID listening on port 3000
+pid=$(lsof -t -i :3000)
+
+# If PID exists, kill the process
+if [ -n "$pid" ]; then
+  echo "Killing process with PID $pid"
+  kill $pid
+else
+  echo "No process listening on port 3000"
+fi
+
 npm ci
 npm start --port 3000
