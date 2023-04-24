6b5d5d30bc31f8dd2c19c6b5bc9a099c915a48c5
Ujjwal <101248846+ujjwalsingh02@users.noreply.github.com>
2023-04-24 16:47:38 +0530

    Resolved GetUserDetails for both doctors and patient fro both fields don't want and for fields needed. (#9)

--- middleware/getUserDetails.js
+++ middleware/getUserDetails.js
@@ -5,20 +5,35 @@ const logs = require("../logs/logs");
 module.exports.getUserDetails = async (req, res) => {
   const { user_id, role, detailsYouNeed, exceptDetailsYouDonNeed } = req.query;
   let fields;
+  const newArray = [];
   if (detailsYouNeed && exceptDetailsYouDonNeed)
     return res.status(200).json(msgHandler.fail(logs[13]));
-  if (detailsYouNeed) fields = detailsYouNeed;
-  console.log(fields);
-  if (exceptDetailsYouDonNeed) fields = `-${exceptDetailsYouDonNeed}`;
+  if (detailsYouNeed) {
+    fields = detailsYouNeed;
+    fields.split(" ").forEach((word) => {
+      newArray.push(word);
+    });
+    console.log(newArray);
+    console.log(fields);
+  }
+  if (exceptDetailsYouDonNeed) {
+    fields = exceptDetailsYouDonNeed;
+    fields.split(" ").forEach((word) => {
+      newArray.push(`-${word}`);
+    });
+    console.log(newArray);
+    console.log(fields);
+  }
 
   await User.findOne(
     {
       _id: user_id,
       role: role,
     },
-    [fields, "-__v"]
+    // [fields, "-__v"]
+    newArray
   )
-    .populate("appointments")
+    // .populate("appointments")
     .exec()
     .then((r) =>
       r
