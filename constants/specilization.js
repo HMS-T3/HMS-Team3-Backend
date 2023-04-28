a2bfb46150ba2da0087fb6b2dd0e40e4b995c8df
Niya <sniya359@gmail.com>
2023-04-28 16:47:20 +0530

    edit specialization

--- constants/specilization.js
+++ constants/specilization.js
@@ -1,102 +1,140 @@
-module.exports = new Object({
-  Immunologists:{Description:
-    "They treat immune system disorders such as asthma, eczema, food allergies, insect sting allergies, and some autoimmune diseases.",
-  ImageLink:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682661888/pills.fill_xzzoki.svg"},
-
-  Anesthesiologists: {Description:
-    "These doctors give you drugs to numb your pain or to put you under during surgery, childbirth, or other procedures. They monitor your vital signs while you’re under anesthesia.",
-  ImageLink:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682662056/Syringe_umlxq6.svg"},
-
-  Cardiologists:{ Description: 
-    "They’re experts on the heart and blood vessels.You might see them for heart failure, a heart attack, high blood pressure, or an irregular heartbeat.",
-    ImageLink:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682662453/heart_ujzvje.svg"},
-
-  Colon_and_Rectal_Surgeons: {Description:
-    "You would see these doctors for problems with your small intestine, colon, and bottom.They can treat colon cancer, hemorrhoids, and inflammatory bowel disease. ",
-    ImageLink: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1682664706/Colon_azolkg.svg"},
-
-  Critical_Care_Medicine_Specialists: {Description:
-    "They care for people who are critically ill or injured, often heading intensive care units in hospitals.You might see them if your heart or other organs are failing or if you’ve been in an accident. ",
-    ImageLink: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1682662657/bed.double.fill_hag6wg.svg"},
-
-  Dermatologists:{ Description:
-    "Have problems with your skin, hair, nails ? Do you have moles, scars, acne, or skin allergies ? Dermatologists can help.",
-    ImageLink:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682664352/Dermis_wsls73.svg"},
-
-  Endocrinologists:{ Description:
-    "These are experts on hormones and metabolism.They can treat conditions like diabetes, thyroid problems, infertility, and calcium and bone disorders.",
-    ImageLink:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682665141/thyroid_inp2y6.svg"},
-
-  Emergency_Medicine_Specialists:{Description:
-    "These doctors make life - or - death decisions for sick and injured people, usually in an emergency room.Their job is to save lives and to avoid or lower the chances of disability.",
-    ImageLink:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682665291/emergency_medicines_fhfsof.svg"},
-
-  Family_Physicians:{Description:
-    "They care for the whole family, including children, adults, and the elderly.They do routine checkups and screening tests, give you flu and immunization shots, and manage diabetes and other ongoing medical conditions.",
-    ImageLink:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682665505/doctor_jascv2.svg"},
-    
-  Gastroenterologists:{Description: 
-    "They are specialists in digestive organs, including the stomach, bowels, pancreas, liver, and gallbladder.You might see them for abdominal pain, ulcers, diarrhea, jaundice, or cancers in your digestive organs.They also do a colonoscopy and other tests for colon cancer.",
-    ImageLink:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682665691/stomach_vi4n5i.svg"},
-
-  Hematologists:{Description:
-    "These are specialists in diseases of the blood, spleen, and lymph glands, like sickle cell disease, anemia, hemophilia, and leukemia.",
-    ImageLink:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682666041/blood_aibon9.svg"},
-
-  Infectious_Disease_Specialists:{Description:
-    "They diagnose and treat infections in any part of your body, like fevers, Lyme disease, pneumonia, tuberculosis, and HIV and AIDS.Some of them specialize in preventive medicine or travel medicine.",
-    ImageLink:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682666253/virus_plmuqb.svg"},
-
-  Medical_Geneticists:{Description:
-    "They diagnose and treat hereditary disorders passed down from parents to children.These doctors may also offer genetic counseling and screening tests.",
-    ImageLink:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682666869/dna_y7xwda.svg"},
-
-  Nephrologists:{Description:
-    "They treat kidney diseases as well as high blood pressure and fluid and mineral imbalances linked to kidney disease.",
-    ImageLink:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682667095/kidney_gd0hbw.svg"},
-
-  Neurologists:{Description:
-    "These are specialists in the nervous system, which includes the brain, spinal cord, and nerves.They treat strokes, brain and spinal tumors, epilepsy, Parkinson's disease, and Alzheimer's disease.",
-    ImageLink:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682667216/brain_myldct.svg"},
-
-  //Oncologists:
-  //"These internists are cancer specialists.They do chemotherapy treatments and often work with radiation oncologists and surgeons to care for someone with cancer.",
-
-  Ophthalmologists:{Description:
-    "You call them eye doctors.They can prescribe glasses or contact lenses and diagnose and treat diseases like glaucoma.Unlike optometrists, they’re medical doctors who can treat every kind of eye condition as well as operate on the eyes.",
-    ImageLink:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682667828/eye_wzoo3q.svg"},
-
-  //Osteopaths:
-  //  "Doctors of osteopathic medicine(DO) are fully licensed medical doctors just like MDs.Their training stresses a “whole body” approach.Osteopaths use the latest medical technology but also the body’s natural ability to heal itself.",
-
-  // Otolaryngologists:
-  //   "They treat diseases in the ears, nose, throat, sinuses, head, neck, and respiratory system.They also can do reconstructive and plastic surgery on your head and neck.",
-
-  // Pathologists:
-  //   "These lab doctors identify the causes of diseases by examining body tissues and fluids under microscopes.",
-
-  // Pediatricians:
-  //   "They care for children from birth to young adulthood.Some pediatricians specialize in pre - teens and teens, child abuse, or children's developmental issues.",
-
-  // Physiatrists:
-  //   "These specialists in physical medicine and rehabilitation treat neck or back pain and sports or spinal cord injuries as well as other disabilities caused by accidents or diseases.",
-
-  // Plastic_Surgeons:
-  //   "You might call them cosmetic surgeons.They rebuild or repair your skin, face, hands, breasts, or body.That can happen after an injury or disease or for cosmetic reasons.",
-
-  // Podiatrists:
-  //   "They care for problems in your ankles and feet.That can include injuries from accidents or sports or from ongoing health conditions like diabetes.Some podiatrists have advanced training in other subspecialties of the foot.",
-
-  // Preventive_Medicine_Specialists:
-  //   "They focus on keeping you well.They may work in public health or at hospitals.Some focus on treating people with addictions, illnesses from exposure to drugs, chemicals, and poisons, and other areas.",
-
-  // Psychiatrists:
-  //   "These doctors work with people with mental, emotional, or addictive disorders.They can diagnose and treat depression, schizophrenia, substance abuse, anxiety disorders, and sexual and gender identity issues.Some psychiatrists focus on children, adolescents, or the elderly.",
-
-  // Pulmonologists:
-  //   "You would see these specialists for problems like lung cancer, pneumonia, asthma, emphysema, and trouble sleeping caused by breathing issues.",
-
-  // Radiologists:
-  //   "They use X - rays, ultrasound, and other imaging tests to diagnose diseases.They can also specialize in radiation oncology to treat conditions like cancer.",
-
-});
+module.exports = [
+	{
+		specialization: "Immunologists",
+		description:
+			"They treat immune system disorders such as asthma, eczema, food allergies, insect sting allergies, and some autoimmune diseases.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682661888/pills.fill_xzzoki.svg",
+	},
+	{
+		specialization: "Anesthesiologists",
+		description:
+			"These doctors give you drugs to numb your pain or to put you under during surgery, childbirth, or other procedures. They monitor your vital signs while you’re under anesthesia.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682662056/Syringe_umlxq6.svg",
+	},
+	{
+		specialization: "Cardiologists",
+		description:
+			"These doctors give you drugs to numb your pain or to put you under during surgery, childbirth, or other procedures. They monitor your vital signs while you’re under anesthesia.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682662453/heart_ujzvje.svg",
+	},
+	{
+		specialization: "Colon_and_Rectal_Surgeons",
+		description:
+			"You would see these doctors for problems with your small intestine, colon, and bottom.They can treat colon cancer, hemorrhoids, and inflammatory bowel disease.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682664706/Colon_azolkg.svg",
+	},
+	{
+		specialization: "Critical_Care_Medicine_Specialists",
+		description:
+			"They care for people who are critically ill or injured, often heading intensive care units in hospitals.You might see them if your heart or other organs are failing or if you’ve been in an accident.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682662657/bed.double.fill_hag6wg.svg",
+	},
+	{
+		specialization: "Dermatologists",
+		description:
+			"Have problems with your skin, hair, nails ? Do you have moles, scars, acne, or skin allergies ? Dermatologists can help.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682664352/Dermis_wsls73.svg",
+	},
+	{
+		specialization: "Endocrinologists",
+		description:
+			"These are experts on hormones and metabolism.They can treat conditions like diabetes, thyroid problems, infertility, and calcium and bone disorders.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682665141/thyroid_inp2y6.svg",
+	},
+	{
+		specialization: "Emergency_Medicine_Specialistss",
+		description:
+			"These doctors make life - or - death decisions for sick and injured people, usually in an emergency room.Their job is to save lives and to avoid or lower the chances of disability.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682665291/emergency_medicines_fhfsof.svg",
+	},
+	{
+		specialization: "Family_Physicians",
+		description:
+			"They care for the whole family, including children, adults, and the elderly.They do routine checkups and screening tests, give you flu and immunization shots, and manage diabetes and other ongoing medical conditions.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682665505/doctor_jascv2.svg",
+	},
+	{
+		specialization: "Gastroenterologists",
+		description:
+			"They are specialists in digestive organs, including the stomach, bowels, pancreas, liver, and gallbladder.You might see them for abdominal pain, ulcers, diarrhea, jaundice, or cancers in your digestive organs.They also do a colonoscopy and other tests for colon cancer.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682665691/stomach_vi4n5i.svg",
+	},
+	{
+		specialization: "Hematologists",
+		description:
+			"These are specialists in diseases of the blood, spleen, and lymph glands, like sickle cell disease, anemia, hemophilia, and leukemia.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682666041/blood_aibon9.svg",
+	},
+	{
+		specialization: "Infectious_Disease_Specialistss",
+		description:
+			"They diagnose and treat infections in any part of your body, like fevers, Lyme disease, pneumonia, tuberculosis, and HIV and AIDS.Some of them specialize in preventive medicine or travel medicine.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682666253/virus_plmuqb.svg",
+	},
+	{
+		specialization: "Medical_Geneticists",
+		description:
+			"They diagnose and treat hereditary disorders passed down from parents to children.These doctors may also offer genetic counseling and screening tests.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682666869/dna_y7xwda.svg",
+	},
+	{
+		specialization: "Nephrologists",
+		description:
+			"They treat kidney diseases as well as high blood pressure and fluid and mineral imbalances linked to kidney disease.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682667095/kidney_gd0hbw.svg",
+	},
+	{
+		specialization: "Neurologists",
+		description:
+			"These are specialists in the nervous system, which includes the brain, spinal cord, and nerves.They treat strokes, brain and spinal tumors, epilepsy, Parkinson's disease, and Alzheimer's disease.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682667216/brain_myldct.svg",
+	},
+	{
+		specialization: "Ophthalmologists",
+		description:
+			"You call them eye doctors.They can prescribe glasses or contact lenses and diagnose and treat diseases like glaucoma.Unlike optometrists, they’re medical doctors who can treat every kind of eye condition as well as operate on the eyes.",
+		imgUrl:
+			"https://res.cloudinary.com/dujgzpuyd/image/upload/v1682667828/eye_wzoo3q.svg",
+	},
+];
+// new Object({
+//Oncologists:
+//"These internists are cancer specialists.They do chemotherapy treatments and often work with radiation oncologists and surgeons to care for someone with cancer.",
+//Osteopaths:
+//  "Doctors of osteopathic medicine(DO) are fully licensed medical doctors just like MDs.Their training stresses a “whole body” approach.Osteopaths use the latest medical technology but also the body’s natural ability to heal itself.",
+// Otolaryngologists:
+//   "They treat diseases in the ears, nose, throat, sinuses, head, neck, and respiratory system.They also can do reconstructive and plastic surgery on your head and neck.",
+// Pathologists:
+//   "These lab doctors identify the causes of diseases by examining body tissues and fluids under microscopes.",
+// Pediatricians:
+//   "They care for children from birth to young adulthood.Some pediatricians specialize in pre - teens and teens, child abuse, or children's developmental issues.",
+// Physiatrists:
+//   "These specialists in physical medicine and rehabilitation treat neck or back pain and sports or spinal cord injuries as well as other disabilities caused by accidents or diseases.",
+// Plastic_Surgeons:
+//   "You might call them cosmetic surgeons.They rebuild or repair your skin, face, hands, breasts, or body.That can happen after an injury or disease or for cosmetic reasons.",
+// Podiatrists:
+//   "They care for probleˀˀms in your ankles and feet.That can include injuries from accidents or sports or from ongoing health conditions like diabetes.Some podiatrists have advanced training in other subspecialties of the foot.",
+// Preventive_Medicine_Specialists:
+//   "They focus on keeping you well.They may work in public health or at hospitals.Some focus on treating people with addictions, illnesses from exposure to drugs, chemicals, and poisons, and other areas.",
+// Psychiatrists:
+//   "These doctors work with people with mental, emotional, or addictive disorders.They can diagnose and treat depression, schizophrenia, substance abuse, anxiety disorders, and sexual and gender identity issues.Some psychiatrists focus on children, adolescents, or the elderly.",
+// Pulmonologists:
+//   "You would see these specialists for problems like lung cancer, pneumonia, asthma, emphysema, and trouble sleeping caused by breathing issues.",
+// Radiologists:
+//   "They use X - rays, ultrasound, and other imaging tests to diagnose diseases.They can also specialize in radiation oncology to treat conditions like cancer.",
+// });
