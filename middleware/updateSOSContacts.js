7ed2f3e8ccfca20bb877d8bfed8b64f75a9f3b0d
aryansingh920 <aryansingh920@outlook.com>
2023-05-01 02:56:52 +0530

    Added by Script

--- middleware/updateSOSContacts.js
+++ middleware/updateSOSContacts.js
@@ -1,14 +1,18 @@
 const User = require("../handler/models.js").User;
 
 const logs = require("../logs/logs");
+const enums = require("../constants/enum");
 const msgHandler = require("../functions/msgHandler");
 
 module.exports.updateSOSContacts = async (req, res) => {
   const { name, phoneNumber, email, relation } = req.body;
-  const { user_id } = req.params;
+  const { user_id } = req.query;
 
   await User.findOneAndUpdate(
-    { _id: user_id },
+    {
+      _id: user_id,
+      role: enums.roles.patient,
+    },
     {
       $push: {
         emergencyContacts: {
