2c9329501e8ae83b65c4db5292851ae0be84e333
aryansingh920 <aryansingh920@outlook.com>
2023-05-03 14:08:26 +0530

    search

--- middleware/search.js
+++ middleware/search.js
@@ -5,32 +5,44 @@ const specialization = require("../constants/specilization.js");
 const enums = require("../constants/enum");
 
 module.exports.search = async (req, res) => {
-  const { searchString } = req.query;
+  const { searchString, searchBy } = req.query;
+
+  let searchQuery = [];
+  if (searchBy === "name")
+    searchQuery = [{ "info.name": { $regex: searchString, $options: "i" } }];
+  else if (searchBy === "specialization")
+    searchQuery = [
+      { "doctorInfo.specialization": { $regex: searchString, $options: "i" } },
+    ];
+  else if (searchBy === "both")
+    searchQuery = [
+      { "info.name": { $regex: searchString, $options: "i" } },
+      {
+        "doctorInfo.specialization": { $regex: searchString, $options: "i" },
+      },
+    ];
 
   if (searchString) {
     const results = await User.find({
-      $or: [
-        { "info.name": { $regex: searchString, $options: "i" } },
-        {
-          "doctorInfo.specialization": { $regex: searchString, $options: "i" },
-        },
-      ],
-    }).then((r) => {
-      let search = [];
+      $or: searchQuery,
+    })
+      .then((r) => {
+        let search = [];
 
-      search = r
-        .filter((a) => a.role === enums.role_doctor)
-        .map((a) => {
-          return {
-            id: a._id,
-            name: a.info.name,
-            specialization: a.doctorInfo.specialization,
-            profileImg: a.info.profileImg,
-            gender: a.info.biologicalGender,
-          };
-        });
+        search = r
+          .filter((a) => a.role === enums.role_doctor)
+          .map((a) => {
+            return {
+              id: a._id,
+              name: a.info.name,
+              specialization: a.doctorInfo.specialization,
+              profileImg: a.info.profileImg,
+              gender: a.info.biologicalGender,
+            };
+          });
 
-      res.status(200).json(msgHandler.pass(search));
-    }).catch(e=>res.status(200).json(msgHandler.fail("Some Error")));
+        res.status(200).json(msgHandler.pass(search));
+      })
+      .catch((e) => res.status(200).json(msgHandler.fail("Some Error")));
   }
 };
