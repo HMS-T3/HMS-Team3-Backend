5151b87510153035f16059dabc0ca1e8fa5e0aa8
aryansingh920 <aryansingh920@outlook.com>
2023-05-03 20:43:20 +0530

    getAppointmentDetails

--- middleware/getAppointmentDetails.js
+++ middleware/getAppointmentDetails.js
@@ -7,7 +7,7 @@ const msgHandler = require("../functions/msgHandler");
 module.exports.getAppointmentDetails = async (req, res) => {
   const { patientId, populate } = req.query;
 
-  const AppointmentDetails = await User.findOne({
+  await User.findOne({
     _id: patientId,
     role: enums.role_patient,
   })
@@ -32,8 +32,21 @@ module.exports.getAppointmentDetails = async (req, res) => {
     })
     .sort({ "appointments.timeSlot.time.endTime": 1 })
     .then((r) => {
-      r.availability.length > 0
-        ? res.status(200).json(msgHandler.pass(r.availability))
+      const events = [...r.appointments];
+
+      events.sort((a, b) => {
+        const dateA = new Date(a.timeSlot.day.split("-").reverse().join("-"));
+        const dateB = new Date(b.timeSlot.day.split("-").reverse().join("-"));
+        const dateComparison = dateA - dateB;
+        if (dateComparison !== 0) {
+          return dateComparison;
+        }
+        const timeA = parseInt(a.timeSlot.time.startTime.replace(":", ""), 10);
+        const timeB = parseInt(b.timeSlot.time.endTime.replace(":", ""), 10);
+        return timeA - timeB;
+      });
+      return r.appointments.length > 0
+        ? res.status(200).json(msgHandler.pass(events))
         : res.status(200).json(msgHandler.pass(null));
     })
     .catch((e) => {
