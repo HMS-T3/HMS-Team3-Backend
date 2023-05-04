91c9b277a164f4405918d9b7bb010fd2eedbf191
Swarup Panda <67518032+swaruppanda11@users.noreply.github.com>
2023-05-04 13:33:40 +0530

    swarrup

--- constants/labTests.js
+++ constants/labTests.js
@@ -2,123 +2,119 @@ module.exports = [
     {
             "test" : "Complete Blood Count (CBC)",
             "description" : "This test measures various components of blood, including red and white blood cells, hemoglobin, and platelets.",
-            "imgUrl" : ""
+            "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683185898/Lab%20Tests/CBC_le47vs.svg"
     },
     {
             "test" : "Blood Glucose Test",
             "description" : "This test measures the level of glucose in the blood and is used to diagnose and monitor diabetes.",
-            "imgUrl" : ""
+            "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683185900/Lab%20Tests/bloodSugar_cc7vq2.svg"
     },
     {
             "test" : "Lipid Panel",
             "description" : "This test measures the levels of different types of cholesterol and triglycerides in the blood, which can help identify the risk of heart disease.",
-            "imgUrl" : ""
+            "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683185968/Lab%20Tests/lipid_jesoqq.svg"
     },
     {
             "test" : "Liver Function Test (LFT)",
             "description" : "This test measures various enzymes and proteins in the blood to evaluate liver function.",
-            "imgUrl" : ""
+            "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683185888/Lab%20Tests/Liver_ygov1m.svg"
     },
     {
             "test" : "Kidney Function Test (KFT)",
             "description" : "This test measures the levels of creatinine and urea in the blood to assess kidney function.",
-            "imgUrl" : ""
+            "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683185891/Lab%20Tests/Kidney_ipbkxy.svg"
     },
     {
             "test" : "Electrocardiogram (ECG or EKG)",
             "description" : "This test measures the electrical activity of the heart and is used to diagnose various heart conditions.",
-            "imgUrl" : ""
+            "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683185896/Lab%20Tests/ecg_jzvmut.svg"
     },
-    {
-            "test" : "Electroencephalogram (EEG)",
-            "description" : "This test measures the electrical activity of the brain and is used to diagnose neurological disorders.",
-            "imgUrl" : ""
-    },
-    {
-            "test" : "Pulmonary Function Test (PFT)",
-            "description" : "This test measures lung capacity and function, and can help diagnose respiratory conditions.",
-            "imgUrl" : ""
-    },
-    {
-            "test" : "Spirometry",
-            "description" : "This is a type of PFT that measures how much air a person can inhale and exhale, and how quickly.",
-            "imgUrl" : ""
-    },
-    {
-            "test" : "Urinalysis",
-            "description" : "This test analyzes urine to evaluate kidney function and detect certain medical conditions.",
-            "imgUrl" : ""
-    },
-    {
-            "test" : "Pap Test",
-            "description" : "This test is used to screen for cervical cancer by collecting cells from the cervix for examination.",
-            "imgUrl" : ""
-    },
-    {
-            "test" : "Mammogram",
-            "description" : "This is an X-ray of the breast tissue and is used to screen for breast cancer.",
-            "imgUrl" : ""
-    },
-    {
-            "test" : "Prostate-Specific Antigen (PSA) Test",
-            "description" : "This test measures the level of PSA in the blood to screen for prostate cancer.",
-            "imgUrl" : ""
-    },
-    {
-            "test" : "Colonoscopy",
-            "description" : "This test uses a camera to examine the colon for signs of cancer or other abnormalities.",
-            "imgUrl" : ""
-    },
-    {
-            "test" : "Endoscopy",
-            "description" : "This test uses a camera to examine the digestive system, including the esophagus, stomach, and intestines.",
-            "imgUrl" : ""
-    },
+   
+    //{
+            //"test" : "Pulmonary Function Test (PFT)",
+            //"description" : "This test measures lung capacity and function, and can help diagnose respiratory conditions.",
+            //"imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683185894/Lab%20Tests/Lungs_cto9bb.svg"
+    //},
+    //{
+    //        "test" : "Spirometry",
+    //        "description" : "This is a type of PFT that measures how much air a person can inhale and exhale, and how quickly.",
+    //        "imgUrl" : ""
+    //},
+    //{
+    //        "test" : "Urinalysis",
+    //        "description" : "This test analyzes urine to evaluate kidney function and detect certain medical conditions.",
+    //        "imgUrl" : ""
+    //},
+    //{
+    //        "test" : "Pap Test",
+    //        "description" : "This test is used to screen for cervical cancer by collecting cells from the cervix for examination.",
+    //        "imgUrl" : ""
+    //},
+    //{
+    //        "test" : "Mammogram",
+    //        "description" : "This is an X-ray of the breast tissue and is used to screen for breast cancer.",
+    //        "imgUrl" : ""
+    //},
+    //{
+    //        "test" : "Prostate-Specific Antigen (PSA) Test",
+    //        "description" : "This test measures the level of PSA in the blood to screen for prostate cancer.",
+    //        "imgUrl" : ""
+    //},
+    //{
+    //        "test" : "Colonoscopy",
+    //        "description" : "This test uses a camera to examine the colon for signs of cancer or other abnormalities.",
+    //        "imgUrl" : ""
+    //},
+    //{
+    //        "test" : "Endoscopy",
+    //        "description" : "This test uses a camera to examine the digestive system, including the esophagus, stomach, and intestines.",
+    //        "imgUrl" : ""
+    //},
     {
             "test" : "Magnetic Resonance Imaging (MRI)",
             "description" : "This test uses a magnetic field and radio waves to create images of internal body structures.",
-            "imgUrl": ""
+            "imgUrl": "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683186426/Lab%20Tests/MRI_ryfe0f.svg"
     },
         
-    {
-        "test": "Mammography",
-        "description": "This is a type of X-ray used to examine the breast tissue and detect any signs of breast cancer.",
-        "imgUrl": "https://www.breastcancer.org/resize/shell/Blog/2021/20210208_mammography/mammo-header-new.png"
-    },
+    //{
+    //    "test": "Mammography",
+    //    "description": "This is a type of X-ray used to examine the breast tissue and detect any signs of breast cancer.",
+    //    "imgUrl": "https://www.breastcancer.org/resize/shell/Blog/2021/20210208_mammography/mammo-header-new.png"
+    //},
         
     {
         "test": "Computed Tomography (CT) Scan",
         "description": "This test uses X-rays and computer technology to create detailed images of internal organs and structures.",
-        "imgUrl": "https://www.radiologyinfo.org/gallery-items/images/0029CT.jpg"
+        "imgUrl": "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683186426/Lab%20Tests/CTscan_muoejt.svg"
     },
         
     {
         "test": "Ultrasound",
         "description": "This test uses high-frequency sound waves to create images of internal organs and tissues.",
-        "imgUrl": "https://www.radiologyinfo.org/gallery-items/images/0004US.jpg"
+        "imgUrl": "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683186885/Lab%20Tests/ultrasound_t7ldmh.svg"
     },
         
     {
         "test": "Bone Density Test",
         "description": "This test measures the density of bones and is used to diagnose osteoporosis.",
-        "imgUrl": "https://www.niams.nih.gov/sites/default/files/inline-images/clinician_bone_density_tests_4col.jpg"
+        "imgUrl": "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683185872/Lab%20Tests/Bone_Joint_cnzfur.svgpg"
     },
         
-    {
-        "test": "Stress Test",
-        "description": "This test measures the heart's response to physical activity or stress and can help diagnose heart conditions.",
-        "imgUrl": "https://www.mayoclinic.org/-/media/kcms/gbs/patient-consumer/images/2013/08/01/15/20/mcdc7_stress_test_feat.jpg"
-    },
+    //{
+    //    "test": "Stress Test",
+    //    "description": "This test measures the heart's response to physical activity or stress and can help diagnose heart conditions.",
+    //    "imgUrl": "https://www.mayoclinic.org/-/media/kcms/gbs/patient-consumer/images/2013/08/01/15/20/mcdc7_stress_test_feat.jpg"
+    //},
         
-    {
-        "test": "Blood Culture",
-        "description": "This test checks for the presence of bacteria or fungi in the blood and is used to diagnose infections.",
-        "imgUrl": "https://www.infectioncontroltoday.com/sites/default/files/styles/article_featured_standard/public/2019-05/istock-1063347736_1.jpg?itok=sTy_QK7E"
-    },
+    //{
+    //    "test": "Blood Culture",
+    //    "description": "This test checks for the presence of bacteria or fungi in the blood and is used to diagnose infections.",
+    //    "imgUrl": "https://www.infectioncontroltoday.com/sites/default/files/styles/article_featured_standard/public/2019-05/istock-1063347736_1.jpg?itok=sTy_QK7E"
+    //},
         
-    {
-        "test": "Thyroid Function Test",
-        "description": "This test measures the levels of thyroid hormones in the blood and is used to diagnose thyroid disorders.",
-        "imgUrl": "https://www.thyroid.org/wp-content/uploads/patients/brochures/FunctionTests_brochure.jpg"
-    },
+    //{
+    //    "test": "Thyroid Function Test",
+    //    "description": "This test measures the levels of thyroid hormones in the blood and is used to diagnose thyroid disorders.",
+    //   "imgUrl": "https://www.thyroid.org/wp-content/uploads/patients/brochures/FunctionTests_brochure.jpg"
+    //},
 ];
--- constants/medicineName.js
+++ constants/medicineName.js
@@ -2,50 +2,50 @@ module.exports = [
    
  {medicineName: "Lipitor (Atorvastatin)",
 description: "A medication used to lower cholesterol levels in the blood. It works by blocking the production of cholesterol in the liver.",
-imgUrl: "https://www.drugs.com/images/pills/mmx/lipitor.jpg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181912/Medicine%20Names/Atorvastatin_dqv43w.svg"
 },
 {medicineName: "Advil (Ibuprofen)",
 description: "A nonsteroidal anti-inflammatory drug (NSAID) used to treat pain and inflammation. It works by reducing hormones that cause inflammation and pain in the body.",
-imgUrl: "https://www.drugs.com/images/pills/fio/advil-gel-caplets.jpg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181910/Medicine%20Names/Advil_f1icw9.svg"
 },
 {medicineName:"Zoloft (Sertraline)",
 description: "An antidepressant medication used to treat depression, obsessive-compulsive disorder, panic disorder, and other mental health conditions. It works by increasing the levels of serotonin in the brain.",
-imgUrl: "https://www.drugs.com/images/pills/pdn/zoloft-100.jpg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181917/Medicine%20Names/Zoloft_arxtje.svg"
 },
  {medicineName:" Plavix (Clopidogrel)",
 description: "A medication used to prevent blood clots in people with heart disease, peripheral vascular disease, and other conditions. It works by preventing platelets from sticking together.",
-imgUrl: "https://www.drugs.com/images/pills/mmx/plavix-75.jpg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181924/Medicine%20Names/plavix_cqt2lz.svg"
  },
 {medicineName: "Tylenol (Acetaminophen)",
 description: "A medication used to relieve pain and reduce fever. It works by blocking the production of certain chemicals that cause pain and fever in the body.",
-imgUrl: "https://www.drugs.com/images/pills/fio/tylenol-arthritis-pain-extended-release.jpg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181932/Medicine%20Names/Tylenol_uu1saa.svg"
 },
 
 {medicineName: "Synthroid (Levothyroxine)",
 description: "A medication used to treat an underactive thyroid gland (hypothyroidism). It works by replacing the hormone that the thyroid gland cannot produce enough of.",
-imgUrl: "https://www.drugs.com/images/pills/fio/synthroid-25.jpg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181920/Medicine%20Names/Synthroid_twamxs.svg"
 },
 
  {medicineName: "Xanax (Alprazolam)",
 description: "A medication used to treat anxiety disorders and panic attacks. It works by enhancing the activity of certain chemicals in the brain that help reduce anxiety.",
-imgUrl:" https://www.drugs.com/images/pills/mmx/xanax-0-5.jpg"
+imgUrl:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181942/Medicine%20Names/Xanax_dyjwss.svg"
  },
 
  {medicineName: "Humira (Adalimumab)",
 description: "A medication used to treat autoimmune disorders such as rheumatoid arthritis, psoriasis, and Crohn's disease. It works by blocking the activity of a protein in the body that causes inflammation."
-,imgUrl: "https://www.drugs.com/images/pills/fio/humira-pen.jpg"
+,imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181940/Medicine%20Names/Humira_cejczd.svg"
 },
 {medicineName: "Glucophage (Metformin)",
 description: "A medication used to treat type 2 diabetes. It works by decreasing the amount of glucose produced by the liver and increasing the body's sensitivity to insulin.",
-imgUrl: "https://www.drugs.com/images/pills/fio/glucophage-xr.jpg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181935/Medicine%20Names/glucophage_xri9r2.svg"
 },
 
  {medicineName:"Prozac (Fluoxetine)",
 description: "An antidepressant medication used to treat depression, obsessive-compulsive disorder, and other mental health conditions. It works by increasing the levels of serotonin in the brain.",
-imgUrl: "https://www.drugs.com/images/pills/fio/prozac-20.jpg"},
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181938/Medicine%20Names/Prozac_inazsw.svg"},
 
 {medicineName: "Cymbalta (Duloxetine)",
 description:" A medication used to treat depression, anxiety, and pain caused by nerve damage in people with diabetes (diabetic neuropathy). It works by increasing the levels of certain chemicals in the brain that help regulate mood and pain.",
-imgUrl: "https://www.drugs.com/images/pills/fio/cymbalta-60.jpg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181929/Medicine%20Names/Cymbalta_mqaloz.svg"
 }
 ]
