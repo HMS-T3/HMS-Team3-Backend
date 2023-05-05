dc6da195ff888fd0c26d20328f1c8d3898a4c05d
Swarup Panda <67518032+swaruppanda11@users.noreply.github.com>
2023-05-05 10:15:36 +0530

    doctors

---
+++ constants/doctors.js
@@ -0,0 +1,82 @@
+module.exports = [
+    {
+        "Gender" : "Male",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683200789/iy8po6u1l2obkdvxu8zs.jpg"
+    },
+    {
+        "Gender" : "Male",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683204154/uynh6rf7rj5euwlcz1xh.png"
+    },
+    {
+        "Gender" : "Male",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683206959/wgv7a05m4w2ndnp5n2oi.jpg"
+    },
+    {
+        "Gender" : "Male",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683207018/doi5sq0sr0cc0s9a667w.jpg"
+    },
+    {
+        "Gender" : "Male",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683207069/e5bv6tkxb1nghd4quiul.jpg"
+    },
+    {
+        "Gender" : "Male",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683207925/hvstmb5fiykiuqsn34so.jpg"
+    },
+    {
+        "Gender" : "Male",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683207958/oqtdl8oseexvxs09dgmq.jpg"
+    },
+    {
+        "Gender" : "Male",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683207990/mttbhmw2uakrnzmw3zow.jpg"
+    },
+    {
+        "Gender" : "Male",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683208024/wdr1pj3rvuk0c7mvbczx.jpg"
+    },
+    {
+        "Gender" : "Male",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683208055/vo0wkblwudtnz3glnk2p.jpg"
+    },
+    {
+        "Gender" : "Female",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683208517/jviiwnlknkypvux9wyg5.jpg"
+    },
+    {
+        "Gender" : "Female",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683208492/gghjdak6tc8cnwrm7bhz.jpg"
+    },
+    {
+        "Gender" : "Female",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683208459/ijuz7elkrozqz6uyscsk.jpg"
+    },
+    {
+        "Gender" : "Female",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683208431/bu7zxkp8ic9zjsrilv7j.jpg"
+    },
+    {
+        "Gender" : "Female",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683208406/zt97ne8fqfw1zzczm4qg.jpg"
+    },
+    {
+        "Gender" : "Female",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683208383/qjhuslxa4pawz1f7rpqd.jpg"
+    },
+    {
+        "Gender" : "Female",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683208307/nuzbjvo5e907xb2j0b4g.jpg"
+    },
+    {
+        "Gender" : "Female",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683208276/lbpnopcw534fudm32j1y.jpg"
+    },
+    {
+        "Gender" : "Female",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683208244/d7lzanresacfbkwatzym.jpg"
+    },
+    {
+        "Gender" : "Female",
+        "imgUrl" : "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683208145/mi6ujns5kj6s5u6xsz3e.jpg"
+    }
+]    
