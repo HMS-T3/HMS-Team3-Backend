3346b87f31dc9c00fff862326db45c1ed66966a8
Swarup Panda <67518032+swaruppanda11@users.noreply.github.com>
2023-05-05 11:48:11 +0530

    medicineName

--- constants/medicineName.js
+++ constants/medicineName.js
@@ -2,50 +2,50 @@ module.exports = [
    
  {medicineName: "Lipitor (Atorvastatin)",
 description: "A medication used to lower cholesterol levels in the blood. It works by blocking the production of cholesterol in the liver.",
-imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181912/Medicine%20Names/Atorvastatin_dqv43w.svg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683267234/Astorvastatin_arbdbq.png"
 },
 {medicineName: "Advil (Ibuprofen)",
 description: "A nonsteroidal anti-inflammatory drug (NSAID) used to treat pain and inflammation. It works by reducing hormones that cause inflammation and pain in the body.",
-imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181910/Medicine%20Names/Advil_f1icw9.svg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683267234/Advil_uday3m.png"
 },
 {medicineName:"Zoloft (Sertraline)",
 description: "An antidepressant medication used to treat depression, obsessive-compulsive disorder, panic disorder, and other mental health conditions. It works by increasing the levels of serotonin in the brain.",
-imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181917/Medicine%20Names/Zoloft_arxtje.svg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683267235/Zoloft_t0pohl.png"
 },
  {medicineName:" Plavix (Clopidogrel)",
 description: "A medication used to prevent blood clots in people with heart disease, peripheral vascular disease, and other conditions. It works by preventing platelets from sticking together.",
-imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181924/Medicine%20Names/plavix_cqt2lz.svg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683267235/Plavix_c6likd.png"
  },
 {medicineName: "Tylenol (Acetaminophen)",
 description: "A medication used to relieve pain and reduce fever. It works by blocking the production of certain chemicals that cause pain and fever in the body.",
-imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181932/Medicine%20Names/Tylenol_uu1saa.svg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683267235/Tylenol_dfdqqw.png"
 },
 
 {medicineName: "Synthroid (Levothyroxine)",
 description: "A medication used to treat an underactive thyroid gland (hypothyroidism). It works by replacing the hormone that the thyroid gland cannot produce enough of.",
-imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181920/Medicine%20Names/Synthroid_twamxs.svg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683267235/Synthroid_jw93um.png"
 },
 
  {medicineName: "Xanax (Alprazolam)",
 description: "A medication used to treat anxiety disorders and panic attacks. It works by enhancing the activity of certain chemicals in the brain that help reduce anxiety.",
-imgUrl:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181942/Medicine%20Names/Xanax_dyjwss.svg"
+imgUrl:"https://res.cloudinary.com/dujgzpuyd/image/upload/v1683267235/Xanax_oici3i.png"
  },
 
  {medicineName: "Humira (Adalimumab)",
 description: "A medication used to treat autoimmune disorders such as rheumatoid arthritis, psoriasis, and Crohn's disease. It works by blocking the activity of a protein in the body that causes inflammation."
-,imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181940/Medicine%20Names/Humira_cejczd.svg"
+,imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683267235/humira_djrrfj.png"
 },
 {medicineName: "Glucophage (Metformin)",
 description: "A medication used to treat type 2 diabetes. It works by decreasing the amount of glucose produced by the liver and increasing the body's sensitivity to insulin.",
-imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181935/Medicine%20Names/glucophage_xri9r2.svg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683267235/glucophage_s9onch.png"
 },
 
  {medicineName:"Prozac (Fluoxetine)",
 description: "An antidepressant medication used to treat depression, obsessive-compulsive disorder, and other mental health conditions. It works by increasing the levels of serotonin in the brain.",
-imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181938/Medicine%20Names/Prozac_inazsw.svg"},
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683267235/Prozac_zv2bfw.png"},
 
 {medicineName: "Cymbalta (Duloxetine)",
 description:" A medication used to treat depression, anxiety, and pain caused by nerve damage in people with diabetes (diabetic neuropathy). It works by increasing the levels of certain chemicals in the brain that help regulate mood and pain.",
-imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683181929/Medicine%20Names/Cymbalta_mqaloz.svg"
+imgUrl: "https://res.cloudinary.com/dujgzpuyd/image/upload/v1683267234/Cymbalta_rloxdm.png"
 }
 ]
